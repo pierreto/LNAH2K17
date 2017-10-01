5e943b44d43bfcd15948342e2be8afa4ad6e5bd0
Ariane Tourangeau <tourangeau.ariane@gmail.com>
2017-10-01 16:07:37 -0400

    3.5.1 hubs created

---
+++ AirHockeyServer/AirHockeyServer/Hubs/GameWaitingRoomHub.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
+using System.Linq;
+using System.Web;
+
+namespace AirHockeyServer.Hubs
+{
+    public class GameWaitingRoomHub : Hub 
+    {
+    }
+}
