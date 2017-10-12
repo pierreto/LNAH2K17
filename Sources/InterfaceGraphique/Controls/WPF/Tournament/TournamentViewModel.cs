58048f94b8027ddc0d6a4baeb14fd73dad178f01
phlel <philippewlelievre@gmail.com>
2017-10-12 17:52:00 -0400

    refs #13850 petit changement

--- Sources/InterfaceGraphique/Controls/WPF/Tournament/TournamentViewModel.cs
+++ Sources/InterfaceGraphique/Controls/WPF/Tournament/TournamentViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace InterfaceGraphique.Controls.WPF.Tournament
+﻿using InterfaceGraphique.CommunicationInterface;
+
+namespace InterfaceGraphique.Controls.WPF.Tournament
 {
     public class TournamentViewModel
     {
+        private WaitingRoomHub waitingRoomHub;
+
+        public TournamentViewModel(WaitingRoomHub waitingRoomHub)
+        {
+            this.waitingRoomHub = waitingRoomHub;
+        }
+
+
     }
 }
