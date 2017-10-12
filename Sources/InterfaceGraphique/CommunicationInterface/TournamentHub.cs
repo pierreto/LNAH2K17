0a8eae8feb8a14609a8713f04e8551d99c6f635e
phlel <philippewlelievre@gmail.com>
2017-10-12 18:32:45 -0400

    refs #13850 added base for tournament view

---
+++ Sources/InterfaceGraphique/CommunicationInterface/TournamentHub.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InterfaceGraphique.Entities;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace InterfaceGraphique.CommunicationInterface
+{
+    class TournamentHub : IBaseHub
+    {
+        private string username;
+        private IHubProxy tournamentHubProxy;
+        public void InitializeHub(HubConnection connection, string username)
+        {
+            this.username = username;
+            tournamentHubProxy = connection.CreateHubProxy("TournamentHub");
+        }
+        public async void JoinTournament()
+        {
+            InitializeEvents();
+            Random random = new Random();
+            UserEntity user = new UserEntity
+            {
+                UserId = random.Next(),
+                Username = username
+            };
+            await tournamentHubProxy.Invoke("JoinGame", user);
+        }
+        private void InitializeEvents()
+        {
+            tournamentHubProxy.On<TournamentOpponentMessage>("OpponentFoundEvent", (opponent) =>
+            {
+
+                tournamentHubProxy.On<GameEntity>("GameStartingEvent", officialGame =>
+                {
+                    
+                });
+            });
+        }
+
+        private void FillTree(TournamentOpponentMessage m)
+        {
+            switch (m)
+            {
+                    
+            }
+        }
+
+        public void Logout()
+        {
+            tournamentHubProxy?.Invoke("Disconnect", this.username).Wait();
+        }
+    }
+}
