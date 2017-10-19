6b927eee6dd1817fc8c9c1e4f72bfb77da60bb97
Mikael Ferland <miferc@l4810-02.gigl.polymtl.ca>
2017-10-18 21:12:03 -0400

    refs #13848 Client léger : ajout des fichiers requis pour la création de murets - Mikael

---
+++ AirHockeyLightClient/AirHockeyLightClient/Noyau/Arbre/Noeuds/NoeudMur.swift
@@ -0,0 +1,64 @@
+///////////////////////////////////////////////////////////////////////////////
+/// @file NoeudMur.swift
+/// @author Mikael Ferland
+/// @date 2017-10-18
+/// @version 1
+///
+/// @addtogroup log3900 LOG3990
+/// @{
+///////////////////////////////////////////////////////////////////////////////
+
+import SceneKit
+
+// TODO : Déplacer dans utilitaire
+struct BoiteEnglobante {
+    var coinMin: GLKVector3?
+    var coinMax: GLKVector3?
+}
+
+///////////////////////////////////////////////////////////////////////////
+/// @class NoeudMur
+/// @brief Classe pour afficher un point de contrôle
+///
+/// @author Mikael Ferland
+/// @date 2017-10-18
+///////////////////////////////////////////////////////////////////////////
+class NoeudMur : NoeudCommun {
+
+    private var collider: BoiteEnglobante?
+    
+    /// Constructeur
+    required init(type: String, geometry: SCNGeometry) {
+        super.init(type: type, geometry: geometry)
+        self.collider = BoiteEnglobante(coinMin: GLKVector3(0, 0, 0), coinMax: GLKVector3(0,0,0));
+    }
+    
+    /// Le point de contrôle a un modèle obj
+    required init(type: String) {
+        fatalError("init(type:geometry:) has not been implemented")
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func obtenirCollider() -> BoiteEnglobante {
+        return self.collider;
+    }
+    
+    // TODO : implémenter collider
+    func calculerCollider() { }
+    
+    /// Cette fonction accepte un visiteur et effectue la bonne methode selon le type
+    override func accepterVisiteur(visiteur: VisiteurAbstrait) {
+        // Envoie le visiteur aux enfants
+        super.accepterVisiteur(visiteur: visiteur)
+        
+        visiteur.visiterPointControl(noeud: self)
+    }
+    
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/// @}
+///////////////////////////////////////////////////////////////////////////////
