802318f296f7dd082ce4fda37fd37a9a499103d1
Mikael Ferland <mikael.ferland@polymtl.ca>
2017-10-22 23:22:34 -0400

    refs #14119 client léger : ajouter une page pour la visualisation des cartes locales - Mikael

---
+++ AirHockeyLightClient/AirHockeyLightClient/MapDisplayViewController.swift
@@ -0,0 +1,83 @@
+///////////////////////////////////////////////////////////////////////////////
+/// @file MapDisplayViewController.swift
+/// @author Mikael Ferland
+/// @date 2017-10-22
+/// @version 1
+///
+/// @addtogroup log3900 LOG3990
+/// @{
+///////////////////////////////////////////////////////////////////////////////
+
+import UIKit
+
+///////////////////////////////////////////////////////////////////////////
+/// @class MapDisplayViewController
+/// @brief Controlleur pour visualiser les cartes sauvegardées localement
+///        et les cartes sauvegardées sur le serveur (si l'utilisateur est
+///        authentifié)
+///
+/// @author Mikael Ferland
+/// @date 2017-09-27
+///////////////////////////////////////////////////////////////////////////
+class MapDisplayViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private var mapsData = [MapEntity]()
+    
+    @IBOutlet weak var maps: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.maps.delegate = self;
+        self.maps.dataSource = self;
+        
+        self.mapsData.append(MapEntity(name: "Default map #1"))
+        self.mapsData.append(MapEntity(name: "Default map #2"))
+        self.mapsData.append(MapEntity(name: "Default map #3"))
+        
+        DispatchQueue.main.async(execute: { () -> Void in
+            // Reload tableView
+            self.maps.reloadData()
+        })
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mapsData.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = maps.dequeueReusableCell(withIdentifier: "Map", for: indexPath);
+        
+        let button = cell.viewWithTag(1) as! UIButton;
+        button.setTitle(mapsData[indexPath.row].getName(), for: UIControlState.normal)
+        button.titleEdgeInsets.left = 20;
+        
+        return cell;
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/// @}
+///////////////////////////////////////////////////////////////////////////////
---
+++ AirHockeyLightClient/AirHockeyLightClient/MapEntity.swift
@@ -0,0 +1,44 @@
+///////////////////////////////////////////////////////////////////////////////
+/// @file MapEntity.swift
+/// @author Mikael Ferland
+/// @date 2017-10-22
+/// @version 1
+///
+/// @addtogroup log3900 LOG3990
+/// @{
+///////////////////////////////////////////////////////////////////////////////
+
+import Foundation
+
+///////////////////////////////////////////////////////////////////////////
+/// @class MapEntity
+/// @brief Classe pour encapsuler une carte
+///
+/// @author Mikael Ferland
+/// @date 2017-10-01
+///////////////////////////////////////////////////////////////////////////
+class MapEntity : Entity {
+    
+    private var name: String
+    private var creationDate: String?
+    private var creator: String?
+    private var type: String?
+    private var json: String?
+    
+    init(name: String) {
+        self.name = name;
+    }
+    
+    public func getName() -> String {
+        return self.name
+    }
+    
+    public func setName(name: String) {
+        self.name = name
+    }
+    
+}
+
+///////////////////////////////////////////////////////////////////////////////
+/// @}
+///////////////////////////////////////////////////////////////////////////////
