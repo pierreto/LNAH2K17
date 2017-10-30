83c8f415151e9591af6550c01927d8bf029d571c
Michaël Sghaier <michael.sghaier@polymtl.ca>
2017-10-30 10:43:10 -0400

    refs #14089 Beginning of the frontend, WIP.

---
+++ Sources/InterfaceGraphique/Controls/WPF/Friends/FriendListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using InterfaceGraphique.CommunicationInterface;
+using InterfaceGraphique.Entities;
+using InterfaceGraphique.CommunicationInterface.RestInterface;
+
+namespace InterfaceGraphique.Controls.WPF.Friends
+{
+    class FriendListViewModel : ViewModelBase
+    {
+    }
+}
