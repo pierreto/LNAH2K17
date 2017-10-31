014dd5d8c08945e241edb0272f6a3e688798f449
Ariane Tourangeau <tourangeau.ariane@gmail.com>
2017-10-31 19:27:59 -0400

    chat service + game Service + cache added

---
+++ AirHockeyServer/AirHockeyServer/Core/Cache.cs
@@ -0,0 +1,20 @@
+﻿using AirHockeyServer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AirHockeyServer.Core
+{
+    public class Cache
+    {
+        public static Dictionary<int, GameEntity> Games;
+        public static Dictionary<int, TournamentEntity> Tournaments;
+
+        public Cache()
+        {
+            Games = new Dictionary<int, GameEntity>();
+            Tournaments = new Dictionary<int, TournamentEntity>();
+        }
+}
+}
---
+++ AirHockeyServer/AirHockeyServer/Manager/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using AirHockeyServer.Core;
+using AirHockeyServer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AirHockeyServer.Manager
+{
+    public class GameManager
+    {
+        //public IPlayerStatsService PlayerStatsService { get; }
+
+        public EventHandler<GameEntity> TournamentUpdateNeeded { get; set; }
+
+        public GameManager()
+        {
+            //PlayerStatsService = new PlayerStatsService();
+        }
+
+        public void AddGame(GameEntity game)
+        {
+            if(!Cache.Games.ContainsKey(game.GameId))
+            {
+                Cache.Games[game.GameId] = game;
+            }
+        }
+
+        public void GoalScored(int gameId, int playerId)
+        {
+            if (Cache.Games.ContainsKey(gameId))
+            {
+                if (Cache.Games[gameId].Players[0].Id == playerId)
+                {
+                    Cache.Games[gameId].Score[0] += 1;
+                }
+                else
+                {
+                    Cache.Games[gameId].Score[1] += 1;
+                }
+            }
+        }
+
+        public void GameEnded(int gameId)
+        {
+            if(Cache.Games.ContainsKey(gameId))
+            {
+                var game = Cache.Games[gameId];
+                game.GameState = GameState.Ended;
+                game.Winner = game.Score[0] > game.Score[1] ? game.Players[0] : game.Players[1];
+                if(game.TournamentId > -1)
+                {
+                    TournamentUpdateNeeded.Invoke(this, game);
+                    //TournamentManager.UpdateTournamentState(game.TournamentId, game);
+                }
+                else
+                {
+                    int points = CaculateGamePoints(Cache.Games[gameId]);
+                    //PlayerStatsService.AddPoints(game.Winner.Id, points);
+                    //PlayerStatsService.IncrementGamesWon(game.Winner.Id);
+                }
+                // TODO Save in DB
+                Cache.Games.Remove(gameId);
+            }
+        }
+        private int CaculateGamePoints(GameEntity gameEntity)
+        {
+            return 20;
+        }
+    }
+}
