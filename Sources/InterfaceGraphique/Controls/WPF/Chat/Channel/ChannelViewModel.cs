6ce6a539628a52963694a3d772cbf4fd720bb891
jealr <jean-marc.al-romhein@polymtl.ca>
2017-10-31 20:04:23 -0400

    Organization du code dans ChannelViewModel

--- Sources/InterfaceGraphique/Controls/WPF/Chat/Channel/ChannelViewModel.cs
+++ Sources/InterfaceGraphique/Controls/WPF/Chat/Channel/ChannelViewModel.cs
@@ -12,137 +12,18 @@ namespace InterfaceGraphique.Controls.WPF.Chat.Channel
 {
     public class ChannelViewModel : ViewModelBase
     {
-
+        #region Private Properties
         private ChatHub chatHub;
-        public ChannelViewModel(ChatHub chatHub)
-        {
-            this.chatHub = chatHub;
-        }
 
-        private ICommand openAddPopupCommand;
-        public ICommand OpenAddPopupCommand
-        {
-            get
-            {
-                if (openAddPopupCommand == null)
-                {
-                    openAddPopupCommand = new RelayCommand(ToggleAddPopup);
-                }
-                return openAddPopupCommand;
-            }
-        }
-
-        private void ToggleAddPopup()
-        {
-            if (IsOpenAdd)
-            {
-                IsOpenAdd = false;
-            } else
-            {
-                IsOpenAdd = true;
-            }
-        }
-
-        private ICommand openOptionsPopupCommand;
-        public ICommand OpenOptionsPopupCommand
-        {
-            get
-            {
-                if (openOptionsPopupCommand == null)
-                {
-                    openOptionsPopupCommand = new RelayCommand(ToggleOptionsPopup);
-                }
-                return openOptionsPopupCommand;
-            }
-        }
-
-        private void ToggleOptionsPopup()
-        {
-            if (IsOpenOptions)
-            {
-                IsOpenOptions = false;
-            }
-            else
-            {
-                IsOpenOptions = true;
-            }
-        }
-
-        private ICommand createChannelCommand;
-        public ICommand CreateChannelCommand
-        {
-            get
-            {
-                if (createChannelCommand == null)
-                {
-                    createChannelCommand = new RelayCommand(CreateChannel);
-                }
-                return createChannelCommand;
-            }
-        }
-
-        private void CreateChannel()
-        {
-            ChannelEntity cE = new ChannelEntity() { Name = Name };
-            ChatListItemViewModel clivm = new ChatListItemViewModel(cE);
-            Program.unityContainer.Resolve<ChatListViewModel>().Items.Add(clivm);
-            OnPropertyChanged("Items");
-            ToggleAddPopup();
-            Name = "";
-
-            //Make sure the previously selected channel is unselected
-            foreach (var item in Program.unityContainer.Resolve<ChatListViewModel>().Items)
-            {
-                item.IsSelected = false;
-            }
-
-            //Set to current channel
-            var clivmList = Program.unityContainer.Resolve<ChatListViewModel>().Items;
-            ActiveChannel.Instance.ChannelEntity = clivmList.Where(s => s.ChannelEntity == cE).First().ChannelEntity;
-            clivm.IsSelected = true;
-            OnPropertyChanged("ChannelSelected");
-
-            chatHub.CreateChannel(cE);
-        }
-
-        private ICommand deleteChannelCommand;
-        public ICommand DeleteChannelCommand
-        {
-            get
-            {
-                if (deleteChannelCommand == null)
-                {
-                    deleteChannelCommand = new RelayCommand(DeleteChannel);
-                }
-                return deleteChannelCommand;
-            }
-        }
-
-        private void DeleteChannel()
-        {
-            var clivm = Program.unityContainer.Resolve<ChatListViewModel>().Items;
-            //Remove the selected ChatListItemViewModel containing the current channel
-            clivm.Remove(clivm.Single(s => s.ChannelEntity == ActiveChannel.Instance.ChannelEntity));
-            OnPropertyChanged("Items");
-            chatHub.LeaveRoom(ActiveChannel.Instance.ChannelEntity.Name);
-            //If possible, set the next channel to the current channel
-            if (clivm.Any())
-            {
-                ActiveChannel.Instance.ChannelEntity = clivm.First().ChannelEntity;
-                clivm.First().IsSelected = true;
-                Program.unityContainer.Resolve<ChatListItemViewModel>().OnPropertyChanged("IsSelected");
-            } else
-            {
-                ActiveChannel.Instance.ChannelEntity = null;
-            }
-            
-            //Refresh the possibility to hide the options button
-            OnPropertyChanged("ChannelSelected");
-            IsOpenOptions = false;
-        }
-
-        //Name of the channel
         private string name;
+
+        private bool isOpenAdd;
+
+        private bool isOpenOptions;
+
+        #endregion
+
+        #region Public Properties
         public string Name
         {
             get { return name; }
@@ -154,7 +35,6 @@ namespace InterfaceGraphique.Controls.WPF.Chat.Channel
             }
         }
 
-        private bool isOpenAdd;
         public bool IsOpenAdd
         {
             get { return isOpenAdd; }
@@ -166,7 +46,6 @@ namespace InterfaceGraphique.Controls.WPF.Chat.Channel
             }
         }
 
-        private bool isOpenOptions;
         public bool IsOpenOptions
         {
             get { return isOpenOptions; }
@@ -184,13 +63,74 @@ namespace InterfaceGraphique.Controls.WPF.Chat.Channel
             get
             {
                 ChannelEntity cE = ActiveChannel.Instance.ChannelEntity;
-                if(Program.unityContainer.Resolve<ChatViewModel>().MainChannel == cE)
+                if (Program.unityContainer.Resolve<ChatViewModel>().MainChannel == cE)
                 {
                     //Disable user from deleting main channel
                     IsOpenOptions = false;
                     return false;
                 }
                 return cE != null;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public ChannelViewModel(ChatHub chatHub)
+        {
+            this.chatHub = chatHub;
+        }
+        #endregion
+
+        #region Commands
+        private ICommand openAddPopupCommand;
+        public ICommand OpenAddPopupCommand
+        {
+            get
+            {
+                if (openAddPopupCommand == null)
+                {
+                    openAddPopupCommand = new RelayCommand(ToggleAddPopup);
+                }
+                return openAddPopupCommand;
+            }
+        }
+
+        private ICommand openOptionsPopupCommand;
+        public ICommand OpenOptionsPopupCommand
+        {
+            get
+            {
+                if (openOptionsPopupCommand == null)
+                {
+                    openOptionsPopupCommand = new RelayCommand(ToggleOptionsPopup);
+                }
+                return openOptionsPopupCommand;
+            }
+        }
+
+        private ICommand createChannelCommand;
+        public ICommand CreateChannelCommand
+        {
+            get
+            {
+                if (createChannelCommand == null)
+                {
+                    createChannelCommand = new RelayCommand(CreateChannel);
+                }
+                return createChannelCommand;
+            }
+        }
+
+        private ICommand deleteChannelCommand;
+        public ICommand DeleteChannelCommand
+        {
+            get
+            {
+                if (deleteChannelCommand == null)
+                {
+                    deleteChannelCommand = new RelayCommand(DeleteChannel);
+                }
+                return deleteChannelCommand;
             }
         }
 
@@ -205,13 +145,6 @@ namespace InterfaceGraphique.Controls.WPF.Chat.Channel
                 }
                 return joinChannelCommand;
             }
-        }
-
-        public async Task JoinChannel()
-        {
-            ChannelEntity cE = await chatHub.JoinChannel("Secondaire");
-            Program.unityContainer.Resolve<ChatListViewModel>().Items.Add(new ChatListItemViewModel(cE));
-            Program.unityContainer.Resolve<ChatListViewModel>().OnPropertyChanged("Items");
         }
 
         private ICommand closeCreateChannelCommand;
@@ -239,6 +172,88 @@ namespace InterfaceGraphique.Controls.WPF.Chat.Channel
                 return closeOptionsCommand;
             }
         }
+        #endregion
+
+        #region Command Methods
+        private void ToggleAddPopup()
+        {
+            if (IsOpenAdd)
+            {
+                IsOpenAdd = false;
+            }
+            else
+            {
+                IsOpenAdd = true;
+            }
+        }
+
+        private void ToggleOptionsPopup()
+        {
+            if (IsOpenOptions)
+            {
+                IsOpenOptions = false;
+            }
+            else
+            {
+                IsOpenOptions = true;
+            }
+        }
+
+        private void CreateChannel()
+        {
+            ChannelEntity cE = new ChannelEntity() { Name = Name };
+            ChatListItemViewModel clivm = new ChatListItemViewModel(cE);
+            Program.unityContainer.Resolve<ChatListViewModel>().Items.Add(clivm);
+            OnPropertyChanged("Items");
+            ToggleAddPopup();
+            Name = "";
+
+            //Make sure the previously selected channel is unselected
+            foreach (var item in Program.unityContainer.Resolve<ChatListViewModel>().Items)
+            {
+                item.IsSelected = false;
+            }
+
+            //Set to current channel
+            var clivmList = Program.unityContainer.Resolve<ChatListViewModel>().Items;
+            ActiveChannel.Instance.ChannelEntity = clivmList.Where(s => s.ChannelEntity == cE).First().ChannelEntity;
+            clivm.IsSelected = true;
+            OnPropertyChanged("ChannelSelected");
+
+            chatHub.CreateChannel(cE);
+        }
+
+        private void DeleteChannel()
+        {
+            var clivm = Program.unityContainer.Resolve<ChatListViewModel>().Items;
+            //Remove the selected ChatListItemViewModel containing the current channel
+            clivm.Remove(clivm.Single(s => s.ChannelEntity == ActiveChannel.Instance.ChannelEntity));
+            OnPropertyChanged("Items");
+            chatHub.LeaveRoom(ActiveChannel.Instance.ChannelEntity.Name);
+            //If possible, set the next channel to the current channel
+            if (clivm.Any())
+            {
+                ActiveChannel.Instance.ChannelEntity = clivm.First().ChannelEntity;
+                clivm.First().IsSelected = true;
+                Program.unityContainer.Resolve<ChatListItemViewModel>().OnPropertyChanged("IsSelected");
+            }
+            else
+            {
+                ActiveChannel.Instance.ChannelEntity = null;
+            }
+
+            //Refresh the possibility to hide the options button
+            OnPropertyChanged("ChannelSelected");
+            IsOpenOptions = false;
+        }
+
+        public async Task JoinChannel()
+        {
+            ChannelEntity cE = await chatHub.JoinChannel("Secondaire");
+            Program.unityContainer.Resolve<ChatListViewModel>().Items.Add(new ChatListItemViewModel(cE));
+            Program.unityContainer.Resolve<ChatListViewModel>().OnPropertyChanged("Items");
+        }
+        #endregion
 
         public override void InitializeViewModel()
         {
