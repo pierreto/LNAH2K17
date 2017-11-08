49475b9ad121b1f968b4c2783c746ffdc1cc6e24
Ariane Tourangeau <tourangeau.ariane@gmail.com>
2017-11-07 23:31:29 -0500

    achivement poco updated

--- AirHockeyServer/AirHockeyServer/Pocos/AchievementPoco.cs
+++ AirHockeyServer/AirHockeyServer/Pocos/AchievementPoco.cs
@@ -9,10 +9,10 @@ namespace AirHockeyServer.Pocos
     [Table(Name = "achievements")]
     public class AchievementPoco 
     {
-        [Column(Name = "user_id")]
+        [Column(Name = "user_id", IsPrimaryKey = true)]
         public int UserId { get; set; }
 
-        [Column(Name = "achievement_type")]
+        [Column(Name = "achievement_type", IsPrimaryKey = true)]
         public string AchievementType { get; set; }
 
         [Column(Name = "is_enabled")]
