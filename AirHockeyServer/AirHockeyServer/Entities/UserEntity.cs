6cb3ef31ee985abe768eb78a7448ee4e33ae4c70
jealr <jean-marc.al-romhein@polymtl.ca>
2017-11-09 20:26:57 -0500

    Web signup Ok!

--- AirHockeyServer/AirHockeyServer/Entities/UserEntity.cs
+++ AirHockeyServer/AirHockeyServer/Entities/UserEntity.cs
@@ -8,5 +8,9 @@ namespace AirHockeyServer.Entities
 
         public string Username { get; set; }
 
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
     }
 }
