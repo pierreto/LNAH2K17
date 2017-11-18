3cf876973fc75ff9fdb4239f4ad0c9e495c986ce
Michael Sghaier <michael.sghaier@polymtl.ca>
2017-11-18 18:44:26 -0500

    Only save icon after a user leaving

--- Sources/InterfaceGraphique/Editor/EditorState/OnlineEditorState.cs
+++ Sources/InterfaceGraphique/Editor/EditorState/OnlineEditorState.cs
@@ -90,7 +90,6 @@ namespace InterfaceGraphique.Editor.EditorState
 
             if (inTransformation)
             {
-                Editeur.mapManager.SaveIcon();
                 Task.Run(() => Editeur.mapManager.SaveMap());
                 this.inTransformation = false;
             }
