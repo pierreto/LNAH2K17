58ec4beb9e1b9c617bee46d1b625c5007f42bede
Philippe Lelièvre <philippe.lelievre@polymtl.ca>
2017-11-19 21:51:35 -0500

    small changes for ui

--- Sources/InterfaceGraphique/Controls/WPF/Editor/EditorViewModel.cs
+++ Sources/InterfaceGraphique/Controls/WPF/Editor/EditorViewModel.cs
@@ -21,6 +21,7 @@ namespace InterfaceGraphique.Controls.WPF.Editor
         private ICommand modeViewCommand;
         private ICommand joinEditionCommand;
 
+        private MapEntity currentMap;
         private MapEntity selectedMap;
 
         public EditorViewModel(MapService mapService)
@@ -91,12 +92,14 @@ namespace InterfaceGraphique.Controls.WPF.Editor
         private async Task JoinEdition()
         {
             await Program.Editeur.JoinEdition(this.selectedMap);
+            currentMap =this.selectedMap;
             Program.EditorHost.Close();
         }
 
         private bool CanJoinEdition()
         {
-            return this.selectedMap!=null && this.selectedMap.CurrentNumberOfPlayer<4;
+
+            return this.selectedMap!=null && this.selectedMap.CurrentNumberOfPlayer<4 && (currentMap==null || currentMap.Id!=this.selectedMap.Id);
         }
 
         public MapEntity SelectedMap
