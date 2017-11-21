dacf071130513e3e94a60a16b500f873c612a913
Mikael Ferland <mikael.ferland@polymtl.ca>
2017-11-20 21:49:45 -0500

    client léger : complete UI template for maps in carousel

--- AirHockeyLightClient/AirHockeyLightClient/MapCarouselViewController.swift
+++ AirHockeyLightClient/AirHockeyLightClient/MapCarouselViewController.swift
@@ -40,10 +40,16 @@ class MapCarouselViewController: UIViewController, iCarouselDataSource, iCarouse
     }
     
     func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
-        let tempView = UIImageView(frame: CGRect(x: 0, y: 0, width: 400, height: 200))
+        let tempView = UIView(frame: CGRect(x: 0, y: 0, width: 400, height: 200))
+        tempView.isUserInteractionEnabled = true
 
+        let mapImage = UIImageView(frame: CGRect(x: 10, y: 10, width: 180, height: 180))
+        mapImage.image = UIImage(named: "map.png")
+        
+        let mapInfo = UIView(frame: CGRect(x: 200, y: 0, width: 200, height: 200))
+        
         let isPublicLabel = UILabel()
-        isPublicLabel.text = "isPublicLabel"
+        isPublicLabel.text = "public"
         isPublicLabel.font = UIFont(name:"HelveticaNeue-Bold", size: 15.0)
         isPublicLabel.textAlignment = NSTextAlignment.right
         isPublicLabel.numberOfLines = 1
@@ -51,15 +57,15 @@ class MapCarouselViewController: UIViewController, iCarouselDataSource, iCarouse
         isPublicLabel.translatesAutoresizingMaskIntoConstraints = false
         
         let mapNameLabel = UILabel()
-        mapNameLabel.text = "mapNameLabel"
-        mapNameLabel.font = UIFont(name:"HelveticaNeue-Bold", size: 15.0)
+        mapNameLabel.text = "batmap"
+        mapNameLabel.font = UIFont.boldSystemFont(ofSize: 30)
         mapNameLabel.textAlignment = NSTextAlignment.right
         mapNameLabel.numberOfLines = 1
         mapNameLabel.textColor = UIColor.black
         mapNameLabel.translatesAutoresizingMaskIntoConstraints = false
         
         let numberOfPlayersLabel = UILabel()
-        numberOfPlayersLabel.text = "numberOfPlayersLabel"
+        numberOfPlayersLabel.text = "0/4"
         numberOfPlayersLabel.font = UIFont(name:"HelveticaNeue-Bold", size: 15.0)
         numberOfPlayersLabel.textAlignment = NSTextAlignment.right
         numberOfPlayersLabel.numberOfLines = 1
@@ -67,7 +73,7 @@ class MapCarouselViewController: UIViewController, iCarouselDataSource, iCarouse
         numberOfPlayersLabel.translatesAutoresizingMaskIntoConstraints = false
         
         let creatorLabel = UILabel()
-        creatorLabel.text = "creatorLabel"
+        creatorLabel.text = "mik"
         creatorLabel.font = UIFont(name:"HelveticaNeue-Bold", size: 15.0)
         creatorLabel.textAlignment = NSTextAlignment.right
         creatorLabel.numberOfLines = 1
@@ -75,8 +81,8 @@ class MapCarouselViewController: UIViewController, iCarouselDataSource, iCarouse
         creatorLabel.translatesAutoresizingMaskIntoConstraints = false
         
         let idLabel = UILabel()
-        idLabel.text = "idLabel"
-        idLabel.font = UIFont(name:"HelveticaNeue-Bold", size: 15.0)
+        idLabel.text = "(17)"
+        idLabel.font = UIFont(name:"HelveticaNeue-Bold", size: 10.0)
         idLabel.textAlignment = NSTextAlignment.right
         idLabel.numberOfLines = 1
         idLabel.textColor = UIColor.black
@@ -87,28 +93,29 @@ class MapCarouselViewController: UIViewController, iCarouselDataSource, iCarouse
         button.addTarget(self, action:#selector(handleRegister(sender:)), for: .touchUpInside)
         
         tempView.addSubview(button)
+        tempView.addSubview(mapImage)
+        tempView.addSubview(mapInfo)
         tempView.addSubview(isPublicLabel)
         tempView.addSubview(mapNameLabel)
         tempView.addSubview(numberOfPlayersLabel)
         tempView.addSubview(creatorLabel)
         tempView.addSubview(idLabel)
         
-        isPublicLabel.centerXAnchor.constraint(equalTo: tempView.centerXAnchor).isActive = true
-        isPublicLabel.topAnchor.constraint(equalTo: tempView.topAnchor, constant: 15).isActive = true
+        isPublicLabel.bottomAnchor.constraint(equalTo: mapInfo.bottomAnchor, constant: -15).isActive = true
+        isPublicLabel.rightAnchor.constraint(equalTo: mapInfo.rightAnchor, constant: -15).isActive = true
         
-        mapNameLabel.centerXAnchor.constraint(equalTo: tempView.centerXAnchor).isActive = true
-        mapNameLabel.topAnchor.constraint(equalTo: tempView.topAnchor, constant: 50).isActive = true
+        mapNameLabel.centerXAnchor.constraint(equalTo: mapInfo.centerXAnchor).isActive = true
+        mapNameLabel.centerYAnchor.constraint(equalTo: mapInfo.centerYAnchor).isActive = true
         
-        numberOfPlayersLabel.centerXAnchor.constraint(equalTo: tempView.centerXAnchor).isActive = true
-        numberOfPlayersLabel.topAnchor.constraint(equalTo: tempView.topAnchor, constant: 90).isActive = true
+        numberOfPlayersLabel.bottomAnchor.constraint(equalTo: mapInfo.bottomAnchor, constant: -15).isActive = true
+        numberOfPlayersLabel.leftAnchor.constraint(equalTo: mapInfo.leftAnchor, constant: 15).isActive = true
         
-        creatorLabel.centerXAnchor.constraint(equalTo: tempView.centerXAnchor).isActive = true
-        creatorLabel.topAnchor.constraint(equalTo: tempView.topAnchor, constant: 130).isActive = true
+        creatorLabel.topAnchor.constraint(equalTo: mapInfo.topAnchor, constant: 15).isActive = true
+        creatorLabel.rightAnchor.constraint(equalTo: mapInfo.rightAnchor, constant: -15).isActive = true
         
-        idLabel.centerXAnchor.constraint(equalTo: tempView.centerXAnchor).isActive = true
-        idLabel.topAnchor.constraint(equalTo: tempView.topAnchor, constant: 170).isActive = true
+        idLabel.centerXAnchor.constraint(equalTo: mapInfo.centerXAnchor).isActive = true
+        idLabel.topAnchor.constraint(equalTo: mapInfo.topAnchor, constant: 115).isActive = true
         
-        tempView.isUserInteractionEnabled = true
         return tempView
     }
     
