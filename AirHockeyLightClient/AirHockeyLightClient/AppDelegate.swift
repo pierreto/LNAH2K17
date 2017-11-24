8f30ce2caa8333e8768322345697ba7bf3d665a4
Pierre To <pierre.to@polymtl.ca>
2017-11-24 17:39:25 -0500

    refs 14391 client léger : gestion de la déconnexion à la fermeture de l'application, on ne supporte pas le background mode

--- AirHockeyLightClient/AirHockeyLightClient/AppDelegate.swift
+++ AirHockeyLightClient/AirHockeyLightClient/AppDelegate.swift
@@ -45,6 +45,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        print("applicationWillResignActive")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -52,21 +53,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         
         // App does not support background running
-        /*if HubManager.sharedConnection.connected! {
-                    deregisterUsername(ipAddress: HubManager.sharedConnection.getIpAddress()!, username: HubManager.sharedConnection.getUsername()!)
-        }*/
+        print("applicationDidEnterBackground")
+        
+        _ = HubManager.sharedConnection.DisconnectUser().then(execute: { response -> Void in
+            _ = HubManager.sharedConnection.StopConnection().then(execute: { response -> Void in
+                print("applicationDidEnterBackground success")
+            })
+        })
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("applicationWillEnterForeground")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("applicationDidBecomeActive")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        print("applicationWillTerminate")
 
         _ = HubManager.sharedConnection.DisconnectUser().then(execute: { response -> Void in
             _ = HubManager.sharedConnection.StopConnection().then(execute: { response -> Void in
