93f345a6fbfff81a863a4710054a2656fe4e9ba5
Ariane Tourangeau-Laberge <artouc@gigl.polymtl.ca>
2017-11-24 22:34:05 -0500

    There's now a message at the end of tournament

--- Sources/InterfaceGraphique/Controls/WPF/Tournament/TournamentViewModel.cs
+++ Sources/InterfaceGraphique/Controls/WPF/Tournament/TournamentViewModel.cs
@@ -102,11 +102,22 @@ namespace InterfaceGraphique.Controls.WPF.Tournament
 
             this.WaitingRoomHub.RemainingTimeEvent += (e, args) => { RemainingTime = args; };
 
-            this.WaitingRoomHub.WinnerResultEvent += (e, args) => { Winner = args.Username; };
+            this.WaitingRoomHub.WinnerResultEvent += (e, args) => OnWinnerResult(e, args) ;
 
             this.WaitingRoomHub.SemiFinalResultEvent += (e, args) => OnSemiFinalResult(e, args);
 
             this.WaitingRoomHub.MapUpdatedEvent += (e, args) => OnMapUpdated(e, args);
+        }
+
+        private void OnWinnerResult(object e, GamePlayerEntity winner)
+        {
+            Winner = winner.Username;
+            WinnerPicture = winner.ProfilePicture;
+            winnerName = winner.Username;
+            isEndOfTournament = true;
+            OnPropertyChanged("IsEndOfTournament");
+
+            EndOfGameTitle = winner.Id == User.Instance.UserEntity.Id ? "Vous Avez Gagné!!!" : "Vous avez perdu :(";
         }
 
         private void OnMapUpdated(object e, MapEntity args)
@@ -279,6 +290,45 @@ namespace InterfaceGraphique.Controls.WPF.Tournament
             set
             {
                 imageSrc = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool isEndOfTournament;
+        public string IsEndOfTournament
+        {
+            get => isEndOfTournament ? "Visible" : "Hidden";
+        }
+
+        private string endOfGameTitle;
+        public string EndOfGameTitle
+        {
+            get => endOfGameTitle;
+            set
+            {
+                endOfGameTitle = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string winnerPicture;
+        public string WinnerPicture
+        {
+            get => winnerPicture;
+            set
+            {
+                winnerPicture = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string winnerName;
+        public string WinnerName
+        {
+            get => winnerName;
+            set
+            {
+                winnerName = value;
                 OnPropertyChanged();
             }
         }
