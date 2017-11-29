1dda4e5243d03067968e8f90a7fd7271256560c3
phlel <philippewlelievre@gmail.com>
2017-11-29 01:08:37 -0500

    quand on creer un nouveau compte et quon acced au store on crash pu

--- Sources/InterfaceGraphique/Controls/WPF/Store/StoreViewModel.cs
+++ Sources/InterfaceGraphique/Controls/WPF/Store/StoreViewModel.cs
@@ -40,10 +40,21 @@ namespace InterfaceGraphique.Controls.WPF.Store
             Load();
             var items = await StoreService.GetStoreItems();
             StoreItems = new ObservableCollection<ItemViewModel>();
-            foreach(var item in items)
+            if (User.Instance.Inventory != null)
             {
-                StoreItems.Add(new ItemViewModel(item, User.Instance.Inventory.Find(x => x.Id == item.Id) == null));
+                foreach (var item in items)
+                {
+                    StoreItems.Add(new ItemViewModel(item, User.Instance.Inventory.Find(x => x.Id == item.Id) == null));
+                }
             }
+            else
+            {
+                foreach (var item in items)
+                {
+                    StoreItems.Add(new ItemViewModel(item, true));
+                }
+            }
+      
 
             var stats = await PlayerStatsService.GetPlayerStats(User.Instance.UserEntity.Id);
             if(stats == null)
